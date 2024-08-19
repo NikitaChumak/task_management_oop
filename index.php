818ede465fb10226e11ea516f8e3f1b6b3c8367f
NikitaChumak <nikitachumak1997@gmail.com>
2024-08-19 15:21:01 +0300

    fix bug

--- index.php
+++ index.php
@@ -6,31 +6,24 @@ use App\Models\Task;
 use App\Models\User;
 use App\Models\Project;
 
-// Создаем пользователя
 $user = new User('John Doe', 'john@example.com');
 
-// Создаем задачи
 $task1 = new Task('Task 1', 'Description for Task 1');
 $task2 = new Task('Task 2', 'Description for Task 2');
 $task1->create();
 $task2->create();
 
-// Назначаем задачи пользователю
 $user->assignTask($task1);
 $user->assignTask($task2);
 
-// Создаем проект
 $project = new Project('Project A');
 
-// Добавляем задачи в проект
 $project->addTask($task1);
 $project->addTask($task2);
 
-// Выводим информацию о проекте и задачах
 echo "Информация о проекте: ";
 var_dump($project->getProjectInfo());
 
-// Удаляем задачу из проекта
 $project->removeTask($task1);
 
 echo "Информация о проекте после удаления задачи: ";
